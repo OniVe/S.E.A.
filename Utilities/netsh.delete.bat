046d6b6051d992fb7508cba2987bcc0657a4ab91
OniVe <mail@onive.ru>
2016-05-26 21:36:00 +0800

    Add Utilities

---
+++ Utilities/netsh.delete.bat
@@ -0,0 +1,13 @@
+@echo off
+set port=9000
+
+echo Administrative permissions required. Detecting permissions...
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	echo Success: Administrative permissions confirmed.
+	netsh http delete urlacl http://+:%port%/
+%USERNAME% listen=yes
+) else (
+	echo Failure: Current permissions inadequate.
+)
+pause >nul
---
+++ Utilities/netsh.update.bat
@@ -0,0 +1,13 @@
+@echo off
+set port=9000
+
+echo Administrative permissions required. Detecting permissions...
+net session >nul 2>&1
+if %errorLevel% == 0 (
+	echo Success: Administrative permissions confirmed.
+	netsh http delete urlacl http://+:%port%/
+	netsh http add urlacl http://+:%port%/ user=%ComputerName%\%USERNAME% listen=yes
+) else (
+	echo Failure: Current permissions inadequate.
+)
+pause >nul
